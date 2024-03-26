bc2532aa46f2522d62aab740eac173d86c72ae3e
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2024-03-26 14:39:42 +0700

    membuat prosedur input

--- FungsiSwitch.cpp
+++ FungsiSwitch.cpp
@@ -3,6 +3,13 @@ using namespace std;
 
 int bilangan1, bilangan2;
 
+void inputData() {
+	cout << "Masukkan bilangan pertama: ";
+	cin >> bilangan1;
+	cout << "Masukkan bilangan kedua: ";
+	cin >> bilangan2;
+}
+
 int main()
 {
 
